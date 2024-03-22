fdb5a1017a76c0ad3371493254be95478ade5441
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-03-22 15:20:17 +0000

    fixed test function

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -2,9 +2,15 @@ from gendiff import generate_diff
 
 
 def test_diff_plain():
-    expected = 'tests/fixtures/plain_json_result.txt'
-    result = generate_diff('tests/fixtures/file1.json',
-                           'tests/fixtures/file2.json')
+    expected_path = 'tests/fixtures/plain_json_result.txt'
+    dict_1_path = 'tests/fixtures/file1.json'
+    dict_2_path = 'tests/fixtures/file2.json'
+
+    with open(expected_path, 'r') as file:
+        expected = file.read()
+
+    result = generate_diff(dict_1_path, dict_2_path)
+
     assert result == expected
 
 
