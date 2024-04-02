2f3ea353c224e42b5f977413849f68a1e39261d6
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-04-02 23:24:27 +0000

    modified generate_diff()

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -14,9 +14,11 @@ def format_value(value):
     return str(value)
 
 
-def format_dict_item(key, tag, value):
+def format_dict_item(element):
+
+    tag, key, value = element
   
-    if tag == MODIFIED:
+    if tag == '±':
         return [f"  - {key}: {format_value(value[0])}", 
                 f"  + {key}: {format_value(value[1])}"]
     
@@ -28,15 +30,15 @@ def gen_diff_string(diff_list):
     return '{\n' + '\n'.join(diff_list) + '\n}'
 
 
-def get_diff(object1, object2):
+def get_diff(dict1, dict2):
         
-    for key in sorted(object1 | object2):
-        val1, val2 = object1.get(key, object()), object2.get(key, object())
+    for key in sorted(dict1.keys() | dict2.keys()):
+        val1, val2 = dict1.get(key, object()), dict2.get(key, object())
 
-        if key not in object2:
+        if key not in dict2:
             yield REMOVED, key, val1
 
-        elif key not in object1:
+        elif key not in dict1:
             yield ADDED, key, val2
 
         elif val1 == val2:
@@ -52,10 +54,10 @@ def format_diff(dict1, dict2):
         yield from format_dict_item(difference)
 
 
-def gen_diff(file1, file2):
+def generate_diff(file1, file2):
 
     file1 = parse_dict(file1)
-    file1 = parse_dict(file2)
+    file2 = parse_dict(file2)
 
     diff = format_diff(file1, file2)
 
