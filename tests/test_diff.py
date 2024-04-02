86574fbd5417b8bb68f9eb51f12ba3e52fde32a3
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-04-02 15:53:51 +0000

    add test for .yml flat files

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,7 +1,7 @@
 from gendiff import generate_diff
 
 
-def test_diff_plain():
+def test_diff_plain_json():
     expected_path = 'tests/fixtures/plain_json_result.txt'
     dict_1_path = 'tests/fixtures/file1.json'
     dict_2_path = 'tests/fixtures/file2.json'
@@ -13,5 +13,17 @@ def test_diff_plain():
 
     assert result == expected
 
+
+def test_diff_plain_yml():
+    expected_path = 'tests/fixtures/plain_json_result.txt'
+    dict_1_path = 'tests/fixtures/file1.yml'
+    dict_2_path = 'tests/fixtures/file2.yml'
+
+    with open(expected_path, 'r') as file:
+        expected = file.read()
+
+    result = generate_diff(dict_1_path, dict_2_path)
+
+    assert result == expected
 
 #  @pytest.mark.parametrize
