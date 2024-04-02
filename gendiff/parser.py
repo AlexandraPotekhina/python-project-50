858d776e5a2028a2aab2fdcbffecc151835122c6
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-04-02 12:57:38 +0000

    add parsing module to parse .yaml and .json files into python object

---
+++ gendiff/parser.py
@@ -0,0 +1,14 @@
+import yaml
+import json
+
+
+def parse_dict(filepath):
+    if filepath.endswith('.yml') or filepath.endswith('.yaml'):
+        with open(filepath, 'r') as file:
+            parsed_dict = yaml.safe_load(file)
+
+    if filepath.endswith('.json'):
+        with open(filepath, 'r') as file:
+            parsed_dict = json.load(open(file))
+
+    return parsed_dict
