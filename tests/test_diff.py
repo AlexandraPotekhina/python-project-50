07001ad7633d541b3fe80f720ca964f3f347d862
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-04-09 13:33:27 +0000

    add test for nested files

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -26,4 +26,17 @@ def test_diff_plain_yml():
 
     assert result == expected
 
+
+def test_diff_nested():
+    expected_path = 'tests/fixtures/result_nested.txt'
+    dict_1_path = 'tests/fixtures/file_nested1.json'
+    dict_2_path = 'tests/fixtures/file_nested2.json'
+
+    with open(expected_path, 'r') as file:
+        expected = file.read()
+
+    result = generate_diff(dict_1_path, dict_2_path)
+
+    assert result == expected
+
 #  @pytest.mark.parametrize
