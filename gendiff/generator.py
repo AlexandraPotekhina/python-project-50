8ec8480ca41f9c52d907a955e5ab9ebedcbd657c
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-04-18 22:49:17 +0000

    modified get_diff and generate_diff

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -1,59 +1,41 @@
 from gendiff.parser import parse_dict
-from gendiff.constants import (ADDED, REMOVED, MODIFIED, SHARED)
-
-
-def format_value(value):
-    if isinstance(value, (bool, type(None))):
-        return str(value).lower()
-
-    return str(value)
-
-
-def format_dict_item(element):
-
-    tag, key, value = element
-  
-    if tag == '±':
-        return [f"  - {key}: {format_value(value[0])}", 
-                f"  + {key}: {format_value(value[1])}"]
-    
-    return [f"  {tag} {key}: {format_value(value)}"]
-
-
-def gen_diff_string(diff_list):
-
-    return '{\n' + '\n'.join(diff_list) + '\n}'
+from gendiff.formatters import get_formatter
+from gendiff.dict_builder import get_diff_dict
+from gendiff.constants import (ADDED, REMOVED, MODIFIED, SHARED, NESTED)
 
 
 def get_diff(dict1, dict2):
-        
-    for key in sorted(dict1.keys() | dict2.keys()):
-        val1, val2 = dict1.get(key, object()), dict2.get(key, object())
 
-        if key not in dict2:
-            yield REMOVED, key, val1
+    def walk(object1, object2, node=[]):
 
-        elif key not in dict1:
-            yield ADDED, key, val2
+        for key in sorted(object1.keys() | object2.keys()):
+            val1, val2 = object1.get(key, object()), object2.get(key, object())
+            new_node = node + [key]
 
-        elif val1 == val2:
-            yield SHARED, key, val2
+            if isinstance(val1, dict) and isinstance(val2, dict):
+                diffs = list(walk(val1, val2, new_node))  
+                if diffs: 
+                    yield NESTED, new_node, diffs
+            elif key not in object2:
+                yield REMOVED, new_node, val1
+            elif key not in object1:
+                yield ADDED, new_node, val2
+            elif val1 == val2:
+                yield SHARED, new_node, val2
+            elif val1 != val2:
+                yield MODIFIED, new_node, (val1, val2)
 
-        elif val1 != val2:
-            yield MODIFIED, key, (val1, val2)
+    return walk(dict1, dict2)
 
 
-def format_diff(dict1, dict2):
-  
-    for difference in get_diff(dict1, dict2):
-        yield from format_dict_item(difference)
-
-
-def generate_diff(file1, file2):
-
+def generate_diff(file1, file2, formatter_style='stylish'):
     file1 = parse_dict(file1)
     file2 = parse_dict(file2)
 
-    diff = format_diff(file1, file2)
+    diff = get_diff(file1, file2)
 
-    return gen_diff_string(diff)
+    diff_dict = get_diff_dict(diff)
+
+    formatter = get_formatter(formatter_style)
+
+    return formatter(diff_dict)
