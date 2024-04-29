391fdac594f5137fde1f5cb17bd921c91b605f9b
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-04-29 14:40:07 +0000

    modified get_diff function

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -4,28 +4,28 @@ from gendiff.dict_builder import get_diff_dict
 from gendiff.constants import (ADDED, REMOVED, MODIFIED, SHARED, NESTED)
 
 
-def get_diff(dict1, dict2):
+def get_diff(dict1, dict2, node=[]):
 
-    def walk(object1, object2, node=[]):
+    # def walk(object1, object2, node=[]):
 
-        for key in sorted(object1.keys() | object2.keys()):
-            val1, val2 = object1.get(key, object()), object2.get(key, object())
+        for key in sorted(dict1.keys() | dict2.keys()):
+            val1, val2 = dict1.get(key, object()), dict2.get(key, object())
             new_node = node + [key]
 
             if isinstance(val1, dict) and isinstance(val2, dict):
-                diffs = list(walk(val1, val2, new_node))  
+                diffs = list(get_diff(val1, val2, new_node))  
                 if diffs: 
                     yield NESTED, new_node, diffs
-            elif key not in object2:
+            elif key not in dict2:
                 yield REMOVED, new_node, val1
-            elif key not in object1:
+            elif key not in dict1:
                 yield ADDED, new_node, val2
             elif val1 == val2:
                 yield SHARED, new_node, val2
             elif val1 != val2:
                 yield MODIFIED, new_node, (val1, val2)
 
-    return walk(dict1, dict2)
+    # return walk(dict1, dict2)
 
 
 def generate_diff(file1, file2, formatter_style='stylish'):
