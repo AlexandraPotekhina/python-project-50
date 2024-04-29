5b6cc0b995cc477df160affab53d9f9c3ae2905d
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-04-29 16:14:53 +0000

    added plain.py module for plain format

---
+++ gendiff/formatters/plain.py
@@ -0,0 +1,24 @@
+def format_value_plain(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    if value in [int, float]:
+        return str(value)
+    if value is None:
+        return 'null'
+    if isinstance(value, dict):
+        return f"[complex value]"
+    return value
+
+
+def format_plain(difference):
+    result = []
+    for tag, key, value in difference:
+        if tag == 'nested':
+            result.append(format_plain(value))
+        elif tag == '+':
+            result.append(f"Property '{'.'.join(key)}' was added with value: '{format_value_plain(value)}'")
+        elif tag == '-':
+            result.append(f"Property '{'.'.join(key)}' was removed")
+        elif tag == 'modified':
+            result.append(f"Property '{'.'.join(key)}' was updated. From '{format_value_plain(value[0])}' to '{format_value_plain(value[1])}'")
+    return '\n'.join(result)
