235b53a7650ec15344dae03ffc80373e99eaff8b
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-05-01 14:39:34 +0000

    add test for plain format

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -27,8 +27,34 @@ def test_diff_plain_yml():
     assert result == expected
 
 
-def test_diff_nested():
+def test_diff_nested_json():
     expected_path = 'tests/fixtures/result_nested.txt'
+    dict_1_path = 'tests/fixtures/file_nested1.json'
+    dict_2_path = 'tests/fixtures/file_nested2.json'
+
+    with open(expected_path, 'r') as file:
+        expected = file.read()
+
+    result = generate_diff(dict_1_path, dict_2_path)
+
+    assert result == expected
+
+
+def test_diff_nested_yml():
+    expected_path = 'tests/fixtures/result_nested.txt'
+    dict_1_path = 'tests/fixtures/file_nested1.yml'
+    dict_2_path = 'tests/fixtures/file_nested2.yml'
+
+    with open(expected_path, 'r') as file:
+        expected = file.read()
+
+    result = generate_diff(dict_1_path, dict_2_path)
+
+    assert result == expected
+
+
+def test_diff_plain():
+    expected_path = 'tests/fixtures/result_plain.txt'
     dict_1_path = 'tests/fixtures/file_nested1.json'
     dict_2_path = 'tests/fixtures/file_nested2.json'
 
