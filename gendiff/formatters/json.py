f6d532fa78ca718c2fac95da8627ce44488d621a
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-05-02 15:58:22 +0000

    minor changes in plain and json formatters

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,4 +1,5 @@
 import json
+from gendiff.constants import (ADDED, REMOVED, MODIFIED, SHARED, NESTED)
 
 
 def get_diff_dict(difference):
@@ -6,29 +7,29 @@ def get_diff_dict(difference):
     new_dict = {}
 
     for tag, key, value in difference:
-        if tag == 'nested':
+        if tag == NESTED:
             new_dict[key[-1]] = get_diff_dict(value)
-        elif tag == 'modified':
+        elif tag == MODIFIED:
             new_dict[key[-1]] = {
                 'type': tag,
                 'old_value': value[0],
                 'new_value': value[1]
             }
-        elif tag == '+':
+        elif tag == ADDED:
             new_dict[key[-1]] = {
                 'type': 'added',
                 'new_value': value
             }
-
-        elif tag == '-':
+        elif tag == REMOVED:
             new_dict[key[-1]] = {
                 'type': 'removed',
                 'old_value': value
             }
-        elif tag == ' ':
+        elif tag == SHARED:
             new_dict[key[-1]] = value
             
     return new_dict
+
 
 def format_string(difference):
     diff = get_diff_dict(difference)
--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,3 +1,6 @@
+from gendiff.constants import (ADDED, REMOVED, MODIFIED, NESTED)
+
+
 def format_value(value):
 
     if isinstance(value, (bool, int, float)):
@@ -14,16 +17,16 @@ def format_string(difference):
     result = []
 
     for tag, key, value in difference:
-        if tag == 'nested':
+        if tag == NESTED:
             result.append(format_string(value))
-        elif tag == '+':
+        elif tag == ADDED:
             result.append(
                 f"Property '{'.'.join(key)}' was added with value: "
                 f"{format_value(value)}"
             )
-        elif tag == '-':
+        elif tag == REMOVED:
             result.append(f"Property '{'.'.join(key)}' was removed")
-        elif tag == 'modified':
+        elif tag == MODIFIED:
             result.append(
                 f"Property '{'.'.join(key)}' was updated. "
                 f"From {format_value(value[0])} "
