21c5c0592667c007e4c2103bb10cada13315113e
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-05-16 12:47:56 +0000

    fix linter errors

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -20,17 +20,21 @@ def format_string(difference, depth=0, space_count=4):
     for tag, key_list, value in difference:
         key = key_list[-1] if key_list else None
         if tag == NESTED:
+            value = format_string(value, depth + 1)
             output.append(
-                f"{indent}  {SHARED} {key}: {format_string(value, depth + 1)}"
+                f"{indent}  {SHARED} {key}: {value}"
             )
         elif tag == MODIFIED:
+            old_value = format_value(value[0], depth + 1)
+            new_value = format_value(value[1], depth + 1)
             output.extend([
-                f"{indent}  {REMOVED} {key}: {format_value(value[0], depth + 1)}",
-                f"{indent}  {ADDED} {key}: {format_value(value[1], depth + 1)}"
+                f"{indent}  {REMOVED} {key}: {old_value}",
+                f"{indent}  {ADDED} {key}: {new_value}"
             ])
         else:
+            value = format_string(value, depth + 1)
             output.append(
-                f"{indent}  {tag} {key}: {format_value(value, depth + 1)}"
+                f"{indent}  {tag} {key}: {value}"
             )
 
     output.append(f"{indent}}}")
